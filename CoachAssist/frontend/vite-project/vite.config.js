7337b337a582cbb54a5c001d15ecb69a2352a5d2
ChickenCoderzzz <110361735+ChickenCoderzzz@users.noreply.github.com>
2026-02-10 23:05:13 -0800

    modified vite.config.js

--- CoachAssist/frontend/vite-project/vite.config.js
+++ CoachAssist/frontend/vite-project/vite.config.js
@@ -1,12 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/auth': 'http://127.0.0.1:8000',
+      "/auth": "http://127.0.0.1:8000",
+      "/players": "http://127.0.0.1:8000",
     },
   },
-})
+});
